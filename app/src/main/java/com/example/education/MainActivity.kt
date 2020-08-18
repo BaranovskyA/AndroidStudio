e76076d98b456371c64603bfc8dedea3e21fbcc1
BaranovskyA <1423demon@mail.ru>
2020-08-18 20:03:16 +0600

    first commit

--- app/src/main/java/com/example/education/MainActivity.kt
+++ app/src/main/java/com/example/education/MainActivity.kt
@@ -1,55 +1,59 @@
 package com.example.education
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
-import android.util.TypedValue
+import android.view.View
 import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.TextView
-
+import android.widget.EditText
+import android.widget.ImageView
+import android.Manifest
+import android.os.Build
+import android.support.annotation.RequiresApi
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var showStudents: TextView
-    lateinit var sortStudents : Button
-    lateinit var randomStudents : Button
-    lateinit var layoutStudents: LinearLayout
-    lateinit var listStudents: Student
+    lateinit var phoneNumber: EditText
+    lateinit var cameraButton: Button
+    lateinit var clickImage: ImageView
+    private val CAMERA = Manifest.permission.CAMERA
+    private val CALL_PHONE = Manifest.permission.CALL_PHONE
+    private val PERMISSION_REQUEST = 200
 
-
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        showStudents = findViewById(R.id.textview_student_list)
-        sortStudents = findViewById(R.id.button_students_sort)
-        randomStudents = findViewById(R.id.button_students_random)
-        layoutStudents = findViewById(R.id.layout_students_list)
-        listStudents = Student()
+        requestPermissions(arrayOf(CAMERA, CALL_PHONE), PERMISSION_REQUEST)
 
-        showAllStudents()
+        phoneNumber = findViewById(R.id.edittext_phone_number)
+        cameraButton = findViewById(R.id.button_camera)
+        clickImage = findViewById(R.id.imageview_camera)
+    }
 
-        sortStudents.setOnClickListener{
-            listStudents.sortStudents()
-            showAllStudents()
-        }
+    fun callOnNumber(view: View) {
+        val toDial = "tel:" + phoneNumber.getText().toString()
+        startActivity(Intent(Intent.ACTION_CALL, Uri.parse(toDial)))
+    }
 
-        randomStudents.setOnClickListener{
-            listStudents.randomStudents()
-            showAllStudents()
+    fun openCamera(view: View) {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            startActivityForResult(takePictureIntent, PERMISSION_REQUEST)
         }
     }
 
-    fun showAllStudents(){
-
-        layoutStudents.removeAllViews()
-
-        listStudents.getAllStudents().forEach {
-            showStudents = TextView(applicationContext)
-            showStudents.text = it
-            showStudents.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20F)
-            layoutStudents.addView(showStudents)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            var photo:Bitmap = data?.extras?.get("data") as Bitmap
+            clickImage.setImageBitmap(photo)
         }
     }
 }
