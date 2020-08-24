589ed90a0f304e20902278c417f75ce9d32daea4
BaranovskyA <1423demon@mail.ru>
2020-08-24 20:27:40 +0600

    first commit

--- app/src/main/java/com/example/education/MainActivity.kt
+++ app/src/main/java/com/example/education/MainActivity.kt
@@ -1,40 +1,67 @@
 package com.example.education
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.support.annotation.RequiresApi
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import android.speech.SpeechRecognizer
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var buttonSend: Button
+    lateinit var speechText: TextView
+    lateinit var buttonSpeech: Button
+    var REQUEST_CODE_SPEECH = 102
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        buttonSend = findViewById(R.id.button_activity_main_send)
-        buttonSend.setOnClickListener {
-            sendMessageWhatsApp()
+        initializeViews()
+        initializeListeners()
+    }
+
+    private fun initializeViews() {
+        speechText = findViewById(R.id.textview_activity_main_text)
+        buttonSpeech = findViewById(R.id.button_activity_main_speech)
+    }
+
+    private fun initializeListeners(){
+        buttonSpeech.setOnClickListener{
+            speak()
         }
     }
 
-    private fun sendMessageWhatsApp() {
-        val shareIntent = Intent(Intent.ACTION_SEND)
-        shareIntent.type = "text/plain"
-        shareIntent.putExtra(
-            Intent.EXTRA_TEXT,
-            "Hello i have written Intent which sending you this message"
-        )
+    private fun speak() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Say something")
 
-        startActivity(
-            Intent.createChooser(
-                shareIntent,
-                "WhatsApp"
-            )
-        )
+        try{
+            startActivityForResult(intent,REQUEST_CODE_SPEECH)
+        }catch (e:Exception){
+            Toast.makeText(this,e.message,Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode){
+            REQUEST_CODE_SPEECH->{
+                if(resultCode == Activity.RESULT_OK && null != data){
+                    val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    speechText.text = result!![0]
+                }
+            }
+        }
     }
 }
