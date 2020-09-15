7531076ec17f467ac82dc478e418d54e15321b06
BaranovskyA <1423demon@mail.ru>
2020-09-15 19:33:21 +0600

    first commit

--- app/src/main/java/com/example/education/data/EducationDatabase.kt
+++ app/src/main/java/com/example/education/data/EducationDatabase.kt
@@ -3,7 +3,7 @@ package com.example.education.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = arrayOf(Student::class), version = 2)
+@Database(entities = arrayOf(Student::class, Subject::class), version = 2)
 abstract class EducationDatabase : RoomDatabase(){
     abstract fun getStudentDao(): StudentDao
 }
--- app/src/main/java/com/example/education/data/StudentDao.kt
+++ app/src/main/java/com/example/education/data/StudentDao.kt
@@ -1,18 +1,44 @@
 package com.example.education.data
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 
 @Dao
 interface StudentDao {
+    @Query("SELECT * FROM students")
+    fun initiateGetStudents() : ArrayList<Student>
+
+    @Query("SELECT * FROM subjects")
+    fun initiateGetSubjects() : ArrayList<Subject>
+
+    @Query("SELECT * FROM students WHERE studentId = id")
+    fun initiateGetStudentById(id: Int) : Student
+
+    @Query("SELECT * FROM subjects WHERE subjectId = id")
+    fun initiateGetSubjectById(id: Int) : Subject
+
+    @Query("DELETE FROM students")
+    fun initiateDeleteStudents()
+
+    @Query("DELETE FROM subjects")
+    fun initiateDeleteSubjects()
+
+    @Query("DELETE FROM students WHERE studentId = id")
+    fun initiateDeleteStudentById(id: Int)
+
+    @Query("DELETE FROM subjects WHERE subjectId = id")
+    fun initiateDeleteSubjectById(id: Int)
+
     @Insert
     fun initiateInsertStudent(student: Student)
 
-    @Delete
-    fun initiateDeleteStudent(student: Student)
+    @Insert
+    fun initiateInsertListStudents(students: ArrayList<Student>)
 
-    @Query("SELECT * FROM students")
-    fun initiateGetStudents() : List<Student>
+    @Insert
+    fun initiateInsertSubject(subject: Subject)
+
+    @Insert
+    fun initiateInsertListSubjects(subjects: ArrayList<Subject>)
 }
